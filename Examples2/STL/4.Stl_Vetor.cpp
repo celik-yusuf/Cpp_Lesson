649638e4f4638a059c6c05ce217ac3259c2279c3
celik-yusuf <celik-yusuf@outlook.com.tr>
2022-05-11 09:48:23 +0300

    Cpp Examples

---
+++ Examples2/STL/4.Stl_Vetor.cpp
@@ -0,0 +1,25 @@
+
+/*
+
+vector = dinamik diziyi uygulayan bir dizi kapsayıcı sınıfıdır; bu, öğe eklerken boyutun otomatik olarak değiştiği anlamına gelir. Bir vektör, öğeleri bitişik bellek konumlarında saklar ve belleği, çalışma zamanında gerektiği gibi tahsis eder.
+
+vector ve dizi arasındaki fark = Bir dizi statik yaklaşımı takip eder, vektörün dinamik diziyi uygularken boyutunun çalışma süresi boyunca değiştirilemeyeceği anlamına gelir, öğeleri eklerken kendisini otomatik olarak yeniden boyutlandırır.
+
+
+*/
+
+#include <iostream>
+#include <vector>
+  
+using namespace std;
+  
+int main()
+{
+
+    vector<string>v1;
+    v1.push_back("Yusuf");
+    v1.push_back("CELİK");
+    
+
+  
+}
